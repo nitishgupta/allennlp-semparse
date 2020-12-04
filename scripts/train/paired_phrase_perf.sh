b0fb06a7042c877290f47f2e75e2ae0decf59bf3
Nitish Gupta <gnnitish@gmail.com>
2020-12-04 15:12:14 -0800

    added v8

--- scripts/train/paired_phrase_perf.sh
+++ scripts/train/paired_phrase_perf.sh
@@ -6,9 +6,10 @@ export OPENBLAS_NUM_THREADS=1
 export OPENMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 
-SPLIT=dev
+SPLIT=test
 
 BASIC_ERM=./resources/checkpoints/nlvr/basicerm_SORT_SD_01/SEED_21/ERM/Iter5_MDS22/predictions/${SPLIT}-predictions.jsonl
+BASIC_ERM2=./resources/checkpoints/nlvr/basicerm_NEWLANG/SEED_1337/ERM/Iter5_MDS22/predictions/${SPLIT}-predictions.jsonl
 
 PAIRED_V1=./resources/checkpoints/nlvr/pairedv1_SORT_SD_T07_P1M2/SEED_42/MML/Iter5_MDS22/predictions/${SPLIT}-predictions.jsonl
 
@@ -16,6 +17,10 @@ PAIRED_V2=./resources/checkpoints/nlvr/pairedv2_NEWLANG_T07_P1M1/SEED_42/ERM/Ite
 PAIRED_V2RE=./resources/checkpoints/nlvr/pairedv2_NEWLANG_T07_P1M2-RE/SEED_42/ERM/Iter5_MDS22/predictions/${SPLIT}-predictions.jsonl
 
 PAIRED_V3=./resources/checkpoints/nlvr/pairedv3_NEWLANG_T07_P1M2/SEED_42/ERM/Iter5_MDS22/predictions/${SPLIT}-predictions.jsonl
+
+PAIRED_V6=./resources/checkpoints/nlvr/pairedv6_NEWLANG_T07_P1M2/SEED_1337/ERM/Iter5_MDS22/predictions/${SPLIT}-predictions.jsonl
+
+PAIRED_V7=./resources/checkpoints/nlvr/pairedv7_NEWLANG_T07_P1M2/SEED_42/ERM/Iter5_MDS22/predictions/${SPLIT}-predictions.jsonl
 
 COMPGEN_BASIC=./resources/checkpoints/nlvr/comp-gen/absstr_v1/basicerm_NEWLANG/SEED_1337/ERM/Iter5_MDS22/predictions/${SPLIT}-predictions.jsonl
 
@@ -26,9 +31,9 @@ COMPGEN_PAIRED_V2=./resources/checkpoints/nlvr/comp-gen/absstr_v1/pairedv2_NEWLA
 COMPGEN_PAIRED_V3=./resources/checkpoints/nlvr/comp-gen/absstr_v1/pairedv3_NEWLANG_T07_P1M1/SEED_1337/ERM/Iter5_MDS22/predictions/${SPLIT}-predictions.jsonl
 
 
-VERSION=v2
+VERSION=v6
 PHRASES=./scripts/nlvr_v2/data/paired_phrases_${VERSION}.json
 
 
-python scripts/nlvr_v2/analysis/phrase_based_performance.py ${PHRASES} ${BASIC_ERM} ${PAIRED_V2RE}
+python scripts/nlvr_v2/analysis/phrase_based_performance.py ${PHRASES} ${BASIC_ERM} ${PAIRED_V6}
 
